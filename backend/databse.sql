144576f3c203e71122fe783cdb5a85cbf61ebefb
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-05 13:47:45 +0700

    Add basic backend setup

---
+++ backend/databse.sql
@@ -0,0 +1,19 @@
+CREATE TABLE category (
+   category_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+   category_name VARCHAR(50),
+   category_image_link VARCHAR(255)
+);
+
+CREATE TABLE item (
+  item_id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  item_name VARCHAR(50),
+  item_desc VARCHAR(100),
+  item_price INTEGER,
+  item_stock INTEGER,
+  item_status BOOLEAN,
+  item_image_link VARCHAR(255),
+  category_id INTEGER,
+  CONSTRAINT fk_category
+    FOREIGN KEY (category_id)
+    REFERENCES category (category_id)
+);
