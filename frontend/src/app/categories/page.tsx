c5ee8f6d0789cbc718b616f6b9c7ba4bb974ab0d
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-08 00:55:06 +0700

    Change routes and add homescreen

---
+++ frontend/src/app/categories/page.tsx
@@ -0,0 +1,36 @@
+import CategoryCard from "@/components/category-card";
+import CategoryTotalCard from "@/components/category-total-card";
+import { categories } from "@/app/data.js";
+
+export default function Categories() {
+  return (
+    <div className="flex">
+      <div className="border-r-2 p-5">
+        <h1 className="py-2">test</h1>
+        <h1 className="py-2">test</h1>
+        <h1 className="py-2">test</h1>
+      </div>
+      <div className="p-5">
+        <h1 className="text-5xl font-light">Categories</h1>
+        <div className="my-5">
+          <CategoryTotalCard
+            name={`Categories`}
+            total={categories.length}
+          />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
+          {categories.map((category) => {
+            // Add return statement or use an implicit return with parentheses
+            return (
+              <CategoryCard
+                key={category.category_id}
+                name={category.category_name}
+                link={category.category_image_link}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/nav-bar.tsx
+++ frontend/src/components/nav-bar.tsx
@@ -1,19 +1,34 @@
-import React from 'react'
+import React from "react";
 import { ModeToggle } from "@/components/mode-toggle";
-import { AddButton } from './add-button';
+import { AddButton } from "./add-button";
+import Link from "next/link";
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between items-center py-4'>
+    <div className="flex justify-between items-center py-4">
       <div>
-        <h1 className='uppercase text-5xl font-tiltNeon text-foreground'>IN<span className='text-primary'>.V</span></h1>
+        <Link href="/">
+          <h1 className="uppercase text-5xl font-tiltNeon text-foreground">
+            IN<span className="text-primary">.V</span>
+          </h1>
+        </Link>
+      </div>
+      <div className="flex gap-5">
+        <Link className="hover:underline text-xl font-light" href="/allitems">
+          All Items
+        </Link>
+        <Link className="hover:underline text-xl font-light" href="/categories">
+          Categories
+        </Link>
       </div>
       <div>
         <AddButton />
-        <span className='ml-2'><ModeToggle/></span>
+        <span className="ml-2">
+          <ModeToggle />
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
