3294599e9a653f1bbb3ef5b663b5f67d6bbe74f7
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-08 23:46:34 +0700

    Add edit and delete item popups for all items

--- frontend/src/app/allitems/page.tsx
+++ frontend/src/app/allitems/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
@@ -17,6 +17,20 @@ import BarAllItemsOne from "@/components/bar-all-items-one";
 import AllItemsDonutChart from "@/components/all-items-donut-chart";
 import AvailDonutChart from "@/components/avail-donut-chart";
 import BarItemPrice from "@/components/bar-item-price";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { Switch } from "@/components/ui/switch";
+import { Textarea } from "@/components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { categories } from "../data";
+import { Plus, Minus } from "lucide-react";
 
 interface Item {
   item_id: number;
@@ -29,6 +43,28 @@ interface Item {
   category_id: number;
   category_name: string;
 }
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface ProcessedItem {
   id: number;
@@ -119,6 +155,28 @@ const Items = () => {
   const barData1 = processDataBar1(allItems);
   const dataPieAvail = getAvailabilitySummary(allItems);
   const barData2 = processDataBar2(allItems);
+  const [price, setPrice] = useState(1);
+  const [stock, setStock] = useState(1);
+
+  const incrementPrice = () => {
+    setPrice(price + 1);
+  };
+
+  const decrementPrice = () => {
+    if (price > 0) {
+      setPrice(price - 1);
+    }
+  };
+
+  const incrementStock = () => {
+    setStock(stock + 1);
+  };
+
+  const decrementStock = () => {
+    if (stock > 0) {
+      setStock(stock - 1);
+    }
+  };
 
   return (
     <div className="flex">
@@ -210,15 +268,174 @@ const Items = () => {
                 </TableCell>
                 <TableCell>
                   <div className="flex justify-center items-center">
-                    <Link href="" className="hover:text-primary">
-                      <PenLine className="transition-transform transform hover:scale-95" />
-                    </Link>
-                    <Link
-                      href=""
-                      className="hover:text-primary text-destructive"
-                    >
-                      <Trash className="transition-transform transform hover:scale-95 ml-2" />
-                    </Link>
+                    <Link href="" className="hover:text-primary"></Link>
+                    <Dialog>
+                      <DialogTrigger asChild>
+                        <PenLine className="hover:text-primary hover:scale-90 cursor-pointer" />
+                      </DialogTrigger>
+                      <DialogContent className="sm:max-w-[425px]">
+                        <DialogHeader>
+                          <DialogTitle>Edit Item "<span className="capitalize">{item.item_name}</span>"</DialogTitle>
+                          <DialogDescription>
+                            Make changes to {item.item_name}. Click edit item when
+                            you're done.
+                          </DialogDescription>
+                        </DialogHeader>
+                        <div className="grid w-full max-w-sm items-center gap-1.5">
+                          <Label htmlFor="name">Name</Label>
+                          <Input
+                            type="text"
+                            id="name"
+                            placeholder="Item Name"
+                            value={item.item_name}
+                          />
+                        </div>
+                        <div className="grid w-full max-w-sm items-center gap-1.5 mt-4">
+                          <Label htmlFor="imageLink">Image Link</Label>
+                          <Input
+                            type="text"
+                            id="imageLink"
+                            placeholder="Image Link"
+                            value={item.item_image_link}
+                          />
+                        </div>
+                        <div className="grid w-full gap-1.5 mt-4">
+                          <Label htmlFor="description">Description</Label>
+                          <Textarea
+                            placeholder="Type your message here."
+                            id="description"
+                            style={{ resize: "none" }}
+                            value={item.item_desc}
+                          />
+                        </div>
+                        <div className="flex flex-col space-y-1.5 mt-4">
+                          <Label htmlFor="category">Item Category</Label>
+                          <Select>
+                            <SelectTrigger id="category">
+                              <SelectValue placeholder="Select Item Category" />
+                            </SelectTrigger>
+                            <SelectContent position="popper">
+                              {categories.map((category) => {
+                                return (
+                                  <SelectItem
+                                    key={category.category_id}
+                                    value={`${category.category_name}`}
+                                  >
+                                    {category.category_name}
+                                  </SelectItem>
+                                );
+                              })}
+                            </SelectContent>
+                          </Select>
+                        </div>
+                        <div className="grid grid-cols-3 gap-4 w-full max-w-sm items-center gap-1.5 mt-4">
+                          <div>
+                            <Label htmlFor="price">Item Price</Label>
+                            <div className="flex items-center gap-2">
+                              <Button
+                                variant="outline"
+                                size="icon"
+                                onClick={decrementPrice}
+                                className="h-10 w-10"
+                                aria-label="Decrease price"
+                              >
+                                <Minus className="h-4 w-4" />
+                              </Button>
+                              <Input
+                                type="number"
+                                id="price"
+                                inputMode="numeric"
+                                step="any"
+                                className="no-arrows h-10 w-12 flex text-center"
+                                value={item.item_price}
+                                onChange={(e) =>
+                                  setPrice(parseInt(e.target.value))
+                                }
+                              />
+                              <Button
+                                variant="outline"
+                                size="icon"
+                                onClick={incrementPrice}
+                                className="h-10 w-10"
+                                aria-label="Increase price"
+                              >
+                                <Plus className="h-4 w-4" />
+                              </Button>
+                            </div>
+                          </div>
+                          <div>
+                            <Label htmlFor="stock">Item Stock</Label>
+                            <div className="flex items-center gap-2">
+                              <Button
+                                variant="outline"
+                                size="icon"
+                                onClick={decrementStock}
+                                className="h-10 w-10"
+                                aria-label="Decrease stock"
+                              >
+                                <Minus className="h-4 w-4" />
+                              </Button>
+                              <Input
+                                type="number"
+                                id="stock"
+                                inputMode="numeric"
+                                step="any"
+                                className="no-arrows h-10 w-12 flex text-center"
+                                value={item.item_stock}
+                                onChange={(e) =>
+                                  setStock(parseInt(e.target.value))
+                                }
+                              />
+                              <Button
+                                variant="outline"
+                                size="icon"
+                                onClick={incrementStock}
+                                className="h-10 w-10"
+                                aria-label="Increase stock"
+                              >
+                                <Plus className="h-4 w-4" />
+                              </Button>
+                            </div>
+                          </div>
+                          <div className="flex flex-col">
+                            <Label htmlFor="">Item Availability</Label>
+                            <Switch checked={item.item_status} className="mt-3" />
+                          </div>
+                        </div>
+                        <DialogFooter>
+                          <div className="flex justify-center">
+                            <Button className="mt-4">
+                              <PenLine /> Edit Item
+                            </Button>
+                          </div>
+                        </DialogFooter>
+                      </DialogContent>
+                    </Dialog>
+                    <AlertDialog>
+                      <AlertDialogTrigger>
+                        <Trash className="transition-transform text-destructive transform hover:scale-95 ml-2" />
+                      </AlertDialogTrigger>
+                      <AlertDialogContent>
+                        <AlertDialogHeader>
+                          <AlertDialogTitle>
+                            Are you absolutely sure you want to delete{" "}
+                            <span className="capitalize">{item.item_name}</span>{" "}
+                            ?
+                          </AlertDialogTitle>
+                          <AlertDialogDescription>
+                            This action cannot be undone. This will permanently
+                            delete {item.item_name} and remove your data from
+                            our servers.
+                          </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                          <AlertDialogCancel>Cancel</AlertDialogCancel>
+                          <AlertDialogAction className="bg-destructive hover:bg-chart-3">
+                            Continue
+                          </AlertDialogAction>
+                        </AlertDialogFooter>
+                      </AlertDialogContent>
+                    </AlertDialog>
                   </div>
                 </TableCell>
               </TableRow>
