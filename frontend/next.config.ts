6ccee2579882e2dc8060ccc0045420ab50de5526
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-26 06:26:55 +0700

    Ignore eslint when buildilng

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['i.imgur.com'], // Add i.imgur.com to the list of allowed domains
   },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during the build
+  },
 };
 
 module.exports = nextConfig;
