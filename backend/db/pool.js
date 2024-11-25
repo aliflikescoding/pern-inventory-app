cdcf975f82d3965118b1284454bdf4c65e9638ec
Alif Wibowo <alif.likes.coding@gmail.com>
2024-11-26 04:07:31 +0700

    Change pool for deployment

--- backend/db/pool.js
+++ backend/db/pool.js
@@ -1,14 +1,10 @@
 import pg from "pg";
 const { Pool } = pg;
-const user = process.env.DB_USER;
-const password = process.env.DB_PASSWORD;
+
+const databaseUrl = process.env.DATABASE_URL;
 
 const pool = new Pool({
-  host: "localhost",
-  user: `${user}`,
-  database: "pern_inventory_app",
-  password: `${password}`,
-  port: 5432,
+  connectionString: databaseUrl,
 });
 
 export default pool;
